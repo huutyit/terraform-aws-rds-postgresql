8695183c69677f74feb7cf337d98ec9fa202c23c
nalbam <me@nalbam.com>
2020-08-21 12:32:28 +0900

    security_group_id

--- 99-output.tf
+++ 99-output.tf
@@ -4,15 +4,11 @@ output "id" {
   value = aws_db_instance.postgresql.id
 }
 
-output "database_security_group_id" {
-  value = aws_security_group.postgresql.id
-}
-
 output "hosted_zone_id" {
   value = aws_db_instance.postgresql.hosted_zone_id
 }
 
-output "hostname" {
+output "address" {
   value = aws_db_instance.postgresql.address
 }
 
@@ -22,4 +18,8 @@ output "port" {
 
 output "endpoint" {
   value = aws_db_instance.postgresql.endpoint
+}
+
+output "security_group_id" {
+  value = aws_security_group.postgresql.id
 }
